922d8ea1d889d501714c51a096fd0b815c68950b
jose antonio rodriguez leandro <alu0100696691@ull.edu.es>
2014-03-26 10:29:40 +0000

    bootstrap instalado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+P6::Application.config.secret_key_base = 'b2fb9b2649f2a38a0c2761c32bfd13a78454f96eb39c503c19402589e9089bdb102ca4979a85c5660c81e7b78c945f71399844e484040ece0f9ab396ac8b063a'
